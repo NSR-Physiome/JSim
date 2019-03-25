6b198b11006306d489ae0771323ae280bf23f3b6
NSR-Physiome <barthj@uw.edu>
2019-03-25 14:42:18 -0700

    Add praxis optimizer algo

--- SRC2.0/JSim/data/OptimArgs.java
+++ SRC2.0/JSim/data/OptimArgs.java
@@ -20,40 +20,40 @@ public class OptimArgs implements Serializable {
 	private int nx;	 // # pars to optimize
 	public String[] xname; // user name for x[i]
 	public double[] xstart; // starting x vals, or NaNs
-	public double[] xmin; // min x vals, or NaNs 
+	public double[] xmin; // min x vals, or NaNs
 	public double[] xmax; // max x vals, or NaNs
 	public int maxCalls; // max # calls (rough)
-	public double errTol; // min error value tolerance 
+	public double errTol; // min error value tolerance
 	public double stepTol; // relative step tolerance
 	public boolean saveLogs; // save x & err value logs?
 	public String matchReport; // desc of data match criteria(if any)
 	public int reportPrec; // output precision in report
 	public boolean calcCovMat; // calc covariance matrix?
 	public double[] confPcts; // confidence fracs (.95=95%)
-	public int maxIters; // maximum # iterations 
+	public int maxIters; // maximum # iterations
 	public int maxStaticIters; // max # iters w/ no improvement
 	public long randomSeed; // <= 0 implies timer seeded
-	
+
 	// simplex specific
 	public double[] xistep; // initial absolute step sizes
 
 	// ggopt specific
-	public double gradTol; // minimum gradient tolerance 
-	public double eps; // relative func error (1e-7 guess???) 
+	public double gradTol; // minimum gradient tolerance
+	public double eps; // relative func error (1e-7 guess???)
 
 	// GridSearch specific
 	public int npoints; // # grid points each dimension
-	
+
 	// SimAnneal specific
 	public double initTemp; // initial "temperature" (must be >0)
 
 	// Genetic algorithm specific
-	public int populationSize;  
+	public int populationSize;
 	public double mutationRate; // 0-1
 	public double crossoverRate; // 0-1
 	public double mutationStep;
-        public int selectMethod;
-        public double eliteCutoff;
+	public int selectMethod;
+	public double eliteCutoff;
 
 	// PSwarm algo specific
 	public int numParticles;  // # of particles in swarm
@@ -63,7 +63,13 @@ public class OptimArgs implements Serializable {
 	public double cogLearn;   // Particle cognitive learning factor
 	public double socLearn;   // Particle social learning factor
 
-	// constructors 
+	// Praxis algo specific
+	public double maxDist; // relative max dist from the init guess to the min
+	public int itrNoImprove;// # of iters without improvement before the algo term
+	public boolean bounds;  // Use bounds for optimization.
+	public double praxisTol; // Max Tolerence between actual cost and current cost.
+
+	// constructors
 	public OptimArgs() { } // RMI constructor
 	public OptimArgs(String alg, int n) {
 	    this(n);
@@ -78,7 +84,7 @@ public class OptimArgs implements Serializable {
 	    xmax = new double[nx];
 	    xistep = new double[nx];
 	    for (int i=0; i<nx; i++) {
-		xstart[i] = xmin[i] = xmax[i] = 
+		xstart[i] = xmin[i] = xmax[i] =
 		xistep[i] = Double.NaN;
 		xname[i] = "x" + i;
 	    }
@@ -103,8 +109,8 @@ public class OptimArgs implements Serializable {
 	    matchReport = a.matchReport;
 	    reportPrec = a.reportPrec;
 	    calcCovMat = a.calcCovMat;
-	    if (a.confPcts != null) 
-	    	confPcts = (double[]) a.confPcts.clone();		
+	    if (a.confPcts != null)
+	    	confPcts = (double[]) a.confPcts.clone();
 	    maxIters = a.maxIters;
 	    maxStaticIters = a.maxStaticIters;
 	    gradTol = a.gradTol;
@@ -122,6 +128,10 @@ public class OptimArgs implements Serializable {
 		maxInertia = a.maxInertia;
 		cogLearn = a.cogLearn;
 		socLearn = a.socLearn;
+		maxDist = a.maxDist;
+		itrNoImprove = a.itrNoImprove;
+		bounds = a.bounds;
+		praxisTol = a.praxisTol;
 	}
 
 	// query
